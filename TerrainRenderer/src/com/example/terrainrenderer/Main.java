04c66e304b07677e8cc303eea832d5072af6d34a
Vivian Liu <viv.liu@mail.utoronto.ca>
2013-08-06 23:43:40 -0400

    Alpha_2.0.0 scrolling capabilities
    - refactored entire project with new title: TerrainRenderer
    - package is now com.example.terrainrenderer
    - renders flat ground with green for 1s and grey for 0s
    - buttons for scrolling left and right
    Operation:
    1. Install and run the app. It should display a single block on the
    bottom left corner of the screen and a message telling you that it can't
    find DemoTerrain.csv. This creates the folder path to the file, but not
    the file itself.
    2. Go into Android/data/com.example.terrainrenderer/Terrain and put
    DemoTerrain.csv there. Note: you don't enjoy the scrolling unless your
    file is big enough to scroll through, so make it have at least 20 ones
    and zeros in it.
    3. Rerun the app, be amazed.

---
+++ TerrainRenderer/src/com/example/terrainrenderer/Main.java
@@ -0,0 +1,53 @@
+package com.example.terrainrenderer;
+
+import com.example.terrainrenderer.R;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+
+public class Main extends Activity implements OnClickListener{
+	private Button leftB, rightB;
+	private AnimatedView aView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.main);
+        
+        // Initialize buttons
+        leftB = (Button)findViewById(R.id.left);
+        leftB.setOnClickListener(this);
+        rightB = (Button)findViewById(R.id.right);
+        rightB.setOnClickListener(this);
+        
+        // Initialize AnimatedView
+        aView = (AnimatedView) findViewById(R.id.anim_view);
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	// If the activity is finishing, shutdown (aka close buffers)
+    	// in AnimatedView
+    	if(isFinishing()) {
+    		aView.shutdown();
+    	}
+    }
+    
+    // Listen for button clicking
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.left:
+			aView.shiftWindow(AnimatedView.LEFT);
+			break;
+		case R.id.right:
+			aView.shiftWindow(AnimatedView.RIGHT);
+			break;
+		}
+	}
+}
